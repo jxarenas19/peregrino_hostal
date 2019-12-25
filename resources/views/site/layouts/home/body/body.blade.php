5d2454c0b041830d5ceaf5243fa33a3571ee2d29
Julio Xavier Arenas Chirino <jxarenas21990@gmail.com>
2019-12-25 15:30:58 -0500

    Puesto algunos textos de forma generica

--- resources/views/site/layouts/home/body/body.blade.php
+++ resources/views/site/layouts/home/body/body.blade.php
@@ -7,10 +7,10 @@
     <div class="container">
         <div class="welcome">
             <div class="section_title nice_title content-center">
-                <h3>Welcome To Hotel</h3>
+                <h3>{{$data['keyWorld']['welcome_hostal']}}</h3>
             </div>
             <div class="section_description">
-                <p> Semper ac dolor vitae accumsan. Cras interdum hendrerit lacinia. Phasellus accumsan urna vitae molestie interdum. Nam sed placerat libero, non eleifend dolor. </p>
+                <p>{{$data['keyWorld']['main_description']}}</p>
             </div>
             @include('site.layouts.home.body.hostal-component')
         </div>
