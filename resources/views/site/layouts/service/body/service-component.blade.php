c5edc114de91f96d65d30e108acfaee5428a18af
Julio Xavier <jxarenas21990@gmail.com>
2020-02-10 23:03:34 -0500

    Agregando mecanismo para mostrar Leer mas

--- resources/views/site/layouts/service/body/service-component.blade.php
+++ resources/views/site/layouts/service/body/service-component.blade.php
@@ -16,10 +16,50 @@
             </div>
             <div class="col-md-5 room_list_desc2">
                 <h6>{{$item['name']}}</h6>
-                <h9>{{$item['description']}}</h9>
+                <span class="more">{{$item['description']}}</span>
 
             </div>
         </div>
     </div><!-- End row room -->
     @endif
 @endforeach
+
+<script>
+    $(document).ready(function() {
+        // Configure/customize these variables.
+        var showChar = 400;  // How many characters are shown by default
+        var ellipsestext = "...";
+        var moretext = "Show more >";
+        var lesstext = "Show less";
+
+
+        $('.more').each(function() {
+            var content = $(this).html();
+            console.log(this);
+            if(content.length > showChar) {
+
+                var c = content.substr(0, showChar);
+                var h = content.substr(showChar, content.length - showChar);
+
+                var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+                $(this).html(html);
+            }
+
+        });
+
+        $(".morelink").click(function(){
+            if($(this).hasClass("less")) {
+                $(this).removeClass("less");
+                $(this).html(moretext);
+            } else {
+                $(this).addClass("less");
+                $(this).html(lesstext);
+            }
+            $(this).parent().prev().toggle();
+            $(this).prev().toggle();
+            return false;
+        });
+    });
+
+</script>
