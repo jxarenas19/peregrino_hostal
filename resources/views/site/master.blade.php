0befedf22dc34116c73b3ae071b1fda1480a16ee
Julio Xavier <jxarenas21990@gmail.com>
2020-02-27 23:48:40 -0500

    Enviado el token de seguridad

--- resources/views/site/master.blade.php
+++ resources/views/site/master.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Hostal Peregrino</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -19,3 +20,10 @@
     @include('site.layouts.footer')
 </body>
 </html>
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
